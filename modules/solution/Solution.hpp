315297fd04a4f621d749c95436cf93691d1bed2f
Yan Yang <yangyan.1988@stu.xjtu.edu.cn>
2012-07-17 17:12:47 +0800

    添加了一个Solution类的框架

---
+++ modules/solution/Solution.hpp
@@ -0,0 +1,23 @@
+#ifndef SOLUTION_CLASS
+#define SOLUTION_CLASS
+
+namespace rv_xjtu_yangyan{
+    class Solution 
+    {
+        public:
+            void clone(Solution &solution);
+    };
+}
+
+namespace rv_xjtu_yangyan{
+
+    void Solution::clone(Solution &solution)
+    {
+    }
+
+}
+
+
+#endif
+
+
