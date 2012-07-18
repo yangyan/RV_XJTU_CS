7404ae0a43c31e2ddf0c2f2439197cc6a03c7301
Yan Yang <yangyan.1988@stu.xjtu.edu.cn>
2012-07-18 15:12:12 +0800

    为Solution类添加了"类型"的框架

--- modules/solution/Solution.hpp
+++ modules/solution/Solution.hpp
@@ -5,11 +5,23 @@ namespace rv_xjtu_yangyan{
     class Solution 
     {
         public:
+            enum
+            {
+                NOT_GIVEN,
+                NO_SOLUTION
+            } type;
+
+        public:
+            Solution();
             void clone(Solution &solution);
     };
 }
 
 namespace rv_xjtu_yangyan{
+
+    Solution::Solution:type(NOT_GIVEN)
+    {
+    }
 
     void Solution::clone(Solution &solution)
     {
