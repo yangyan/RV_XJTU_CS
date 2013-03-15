3f01653660118d29fd7b2e1144645abca06faf28
Yan Yang <yangyan.1988@stu.xjtu.edu.cn>
2013-03-15 18:52:46 +0800

    做了一些模块化的事情，下一步对真的程序进行容错吧

---
+++ test/modulized/main.cpp
@@ -0,0 +1,22 @@
+#include  <iostream>
+#include  <cstdlib>
+using namespace std;
+
+void often(int);
+
+
+void often(int n)
+{
+    int *a = (int *)malloc(100);
+    free(a);
+    free(a);
+    free(a);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    often(0);
+    return 0;
+}
