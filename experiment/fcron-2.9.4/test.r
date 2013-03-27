0bd0c57b1f49b142ffee151966c6e62268d9ac99
Yan Yang <yangyan.1988@stu.xjtu.edu.cn>
2013-03-27 14:35:29 +0800

    稍微修改了experiment1的配置文件，使其正确运行

--- experiment/fcron-2.9.4/test.r
+++ experiment/fcron-2.9.4/test.r
@@ -1,7 +1,7 @@
 Rule fcrontab(open_and_close){
 	within("f_is_allowed()"){
 		key fd;
-		"f_fopen(fd)" & (N "f_fclose(fd)");
+		G("f_fopen(fd)" -> (N "f_fclose(fd)"));
 	}solution_doclose(fd) && stop();
 }
 
